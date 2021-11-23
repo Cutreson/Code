ae9c4ae7c6742dd5eed8850b267a557a3ad7e7ca
dinhhongsonpt <dinhhongsontbpt@gmail.com>
2021-11-23 17:26:50 +0700

    23/11

--- NhanDien.py
+++ NhanDien.py
@@ -6,37 +6,42 @@ from PIL import Image
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-
 recognizer.read("recoginizer/trainningData.yml")
-def getProfile(SoTu):
-    conn = sqlite3.connect("database.db")
-    query = "SELECT * FROM data WHERE SoTu = " + str(SoTu)
-    cusror = conn.execute(query)
-    profile = None
-    for row in cusror:
-        profile = row
-    conn.close()
-    return profile
 ############################################################
+class NhanDien():
+    def __init__(self) -> None:
+        pass
 
-def nhanDien():
-    cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray,1.3,5)
-    for(x,y,w,h) in faces:
-        roi_gray = gray[y:y+h,x:x+w]
-        SoTu,confidence = recognizer.predict(roi_gray)
-        if confidence < 30:
-            profile = getProfile(SoTu)
-            if(profile != None):
-                print("True")
+    def getProfile(self,SoTu):
+        conn = sqlite3.connect("database.db")
+        query = "SELECT * FROM data WHERE SoTu = " + str(SoTu)
+        cusror = conn.execute(query)
+        profile = None
+        for row in cusror:
+            profile = row
+        conn.close()
+        return profile
+    ############################################################
+
+    def nhanDien(self):
+        cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray,1.3,5)
+        for(x,y,w,h) in faces:
+            roi_gray = gray[y:y+h,x:x+w]
+            SoTu,confidence = recognizer.predict(roi_gray)
+            if confidence < 70:
+                profile = self.getProfile(SoTu)
+                if(profile != None):
+                    print("True")
+                    cap.release()
+                    return True
+            else:
+                print("False")
                 cap.release()
-                return True
-        else:
-            print("False")
-            cap.release()
-            return False
+                return False
 
 #####################################################
-nhanDien()
+nhanDien = NhanDien()
+nhanDien.nhanDien()
---
+++ main.py
@@ -0,0 +1,114 @@
+import sys
+import cv2
+from time import sleep
+import cv2
+import numpy as np
+import sqlite3
+from PIL import Image
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QThread, pyqtSignal, Qt
+from PyQt5.QtCore import QThread, pyqtSignal, Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from GUI import Ui_MainWindow
+#from Data import *
+#from Trainning import *
+###########################################
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("recoginizer/trainningData.yml")
+#############################################
+class capture_video(QThread):
+    signal = pyqtSignal(np.ndarray)
+    def __init__(self, index):
+        self.index = index
+        print("start threading", self.index)
+        super(capture_video, self).__init__()
+
+    def run(self):
+        cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)       
+        while True:
+            ret, cv_img = cap.read()
+            gray = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(cv_img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+            if ret:
+                self.signal.emit(cv_img)
+
+    def stop(self):
+        print("stop threading", self.index)
+        self.terminate()
+#############################################
+
+class NhanDien():
+    def __init__(self) -> None:
+        pass
+
+    def getProfile(self,SoTu):
+        conn = sqlite3.connect("database.db")
+        query = "SELECT * FROM data WHERE SoTu = " + str(SoTu)
+        cusror = conn.execute(query)
+        profile = None
+        for row in cusror:
+            profile = row
+        conn.close()
+        return profile
+    ############################################################
+
+    def nhanDien(self):
+        cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray,1.3,5)
+        for(x,y,w,h) in faces:
+            roi_gray = gray[y:y+h,x:x+w]
+            SoTu,confidence = recognizer.predict(roi_gray)
+            if confidence < 70:
+                profile = self.getProfile(SoTu)
+                if(profile != None):
+                    print("True")
+                    cap.release()
+                    return True
+            else:
+                print("False")
+                cap.release()
+                return False
+    
+############################################
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.uic = Ui_MainWindow()
+        self.uic.setupUi(self)
+        self.thread = {}
+
+    def closeEvent(self, event):
+        self.stop_capture_video()
+
+    def stop_capture_video(self):
+        self.thread[1].stop()
+
+    def start_capture_video(self):
+        self.thread[1] = capture_video(index=1)
+        self.thread[1].start()
+        self.thread[1].signal.connect(self.show_wedcam)
+
+    def show_wedcam(self, cv_img):
+        qt_img = self.convert_cv_qt(cv_img)
+        self.uic.label_Camera.setPixmap(qt_img)
+
+    def convert_cv_qt(self, cv_img):
+        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+        p = convert_to_Qt_format.scaled(800, 600, Qt.KeepAspectRatio)
+        return QPixmap.fromImage(p)
+##############################################
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    main_win.start_capture_video()
+    sys.exit(app.exec())
