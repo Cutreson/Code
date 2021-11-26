5d55736d97e627aaa0913251a748116ddff2524a
Cutreson <sondh@svrqo.com>
2021-11-26 22:46:53 +0700

    ok

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ import time
 import playsound
 from datetime import datetime
 import serial
+import serial.tools.list_ports
 
-port = serial.Serial('COM5',9600)
 duration = 3
 Confidence = 50
 time_Open = 2
@@ -27,6 +27,30 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fronta
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("recoginizer/trainningData.yml")
 #############################################
+def check_port():
+    serPort = ""
+    int1 = 0
+    str1 = ""
+    str2 = ""
+    # Find Live Ports
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+    #print (p) # This causes each port's information to be printed out.
+            # To search this p data, use p[1].
+        while (int1 < 9):   # Loop checks "COM0" to "COM8" for Adruino Port Info. 
+            if "CH340" in p[1]:  # Looks for "CH340" in P[1].
+                    str2 = str(int1) # Converts an Integer to a String, allowing:
+                    str1 = "COM" + str2 # add the strings together
+            if "CH340" in p[1] and str1 in p[1]: # Looks for "CH340" and "COM#"
+                print ("Found Arduino Uno on " + str1)
+                int1 = 9 # Causes loop to end.
+            if int1 == 8:
+                print ("UNO not found!")
+                sys.exit() # Terminates Script.
+            int1 = int1 + 1
+    return str1
+port = serial.Serial(check_port(),9600)
+##############################################
 def voice_gui_do(SoTu):
     if(SoTu == 1):
         playsound.playsound("voice_gui_do_1.mp3")
